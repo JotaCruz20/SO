3e6624d414666e741b6328d4c8f0a6e57c972a05
DreSilva <2000.andre.silva@gmail.com>
2019-11-05 22:06:52 +0000

    UP STAT LOG

--- statistics_log.c
+++ statistics_log.c
@@ -31,3 +31,39 @@ char* current_time(){
   sprintf(stime,"%d:%d:%d",timeinfo->tm_hour,timeinfo->tm_min,timeinfo->tm_sec);
   return stime;
 }
+
+int verify_command(char* command){
+  char token[10],token1[10],token2[10],token3[10],token4[10],token5[10];
+  int flag,i;
+  token=strtok(command," ");
+  if(strcmp(token,"ARRIVAL")==0){
+    token=strtok(NULL," ");
+    if(token[0]='T' && token[1]='P'){
+      for(i=2;token[i]!='\0';i++){
+        if(token[i]>48 || token[i]<57)
+          return 0;
+      }
+      token=strtok(NULL," ");
+      token1=strtok(NULL," ");
+      token2=strtok(NULL," ");
+      token3=strtok(NULL," ");
+      token4=strtok(NULL," ");
+      token5=strtok(NULL," ");
+      if(strcmp())
+    }
+    else
+      return 0;
+
+  }
+  else if(strcmp(token,"DEPARTURE")==0){
+    token=strtok(NULL," ");
+    if(token[0]='T' && token[1]='P'){
+      //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    }
+    else
+      return 0;
+  }
+  else
+    return 0;
+}

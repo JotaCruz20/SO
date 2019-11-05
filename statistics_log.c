6abc6beeee764eb2c95b5eba22214a3826807d4a
DreSilva <2000.andre.silva@gmail.com>
2019-11-05 18:43:24 +0000

    Up sta log

--- statistics_log.c
+++ statistics_log.c
@@ -1,4 +1,4 @@
-include "statistics.h"
+include "statistics_log.h"
 void update_statistic(p_sta statistic){
   statistic->average_wait_time_landing=statistics->sum_wait_time_landing/statistics->landed_flights;
   statistic->average_wait_time_taking_of=statistics->sum_wait_time_taking_of/statistics->take_of_flights;
--- statistics_log.h
+++ statistics_log.h
@@ -14,4 +14,6 @@ typedef struct{
 }Statistic
 typedef Statistic* p_sta;
 
+
+
 void update_statistic();
